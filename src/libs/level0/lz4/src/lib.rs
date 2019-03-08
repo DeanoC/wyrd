7ad954762de991e352a59785739bd9f0888771cf
deano <me@deanoc.com>
2019-03-08 22:24:41 +0200

    renamed cpp_src to src moved rust_src into src rust source now lives in same directory structure as c and c++ source

---
+++ src/libs/level0/lz4/src/lib.rs
@@ -0,0 +1,25 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+use std::ffi::CStr;
+
+pub fn version() -> i32 {
+    return unsafe { LZ4_versionNumber() }
+}
+pub fn version_string() -> String {
+    unsafe {
+        CStr::from_ptr(LZ4_versionString()).to_string_lossy().into_owned()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn major_version_chk() {
+        assert_eq!(version(), 10802);
+    }
+}
